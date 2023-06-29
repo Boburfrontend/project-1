40268a16fa284a443f596f9c7551e8cce2df940a
Boburfrontend <example@gmail.com>
2023-06-29 10:00:03 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+///BMI=body mass index
+///BMI=weight/(height*height)
+///bmi<14.99 underweight;15.00<bmi<24.99 normal;25<overweiht;
+
+var imonaweight = 23;
+var imonaheight = 1.2;
+var imonaBMI = imonaweight / (imonaheight * imonaheight);
+console.log(imonaBMI);
